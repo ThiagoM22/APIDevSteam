37cc97773638228016dddc86cf6f77cd7bfcf677
Thiago Mazzi <thiagodev728@gmail.com>
2025-04-16 11:59:33 -0300

    Adicionar arquivos de projeto.

---
+++ APIDevSteam/Data/APIDevSteamContext.cs
@@ -0,0 +1,25 @@
+﻿using APIDevSteam.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIDevSteam.Data
+{
+    public class APIDevSteamContext : IdentityDbContext<Usuario>
+    {
+        public APIDevSteamContext(DbContextOptions<APIDevSteamContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+
+
+    }
+}
